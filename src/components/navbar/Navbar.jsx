6cea92c1ae5961cb9c4c4af3a11f5b7e9458e4f4
Emranfaishal <emranfaishalrahat@gmail.com>
2026-04-13 22:46:13 +0600

    navbar and footer add

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,15 +1,29 @@
 import React from 'react';
+import { NavLink } from 'react-router';
+import { FaHome } from "react-icons/fa";
+import { IoTimeOutline } from "react-icons/io5";
+import { TfiStatsUp } from "react-icons/tfi";
 
 const Navbar = () => {
     return (
-        <div className="navbar bg-base-100 shadow-sm">
-            <div className="flex-1">
-                <a className="btn btn-ghost text-xl">daisyUI</a>
-            </div>
-            <div className="flex-none">
-                <button className='btn'>Home</button>
-                <button className='btn'>Home</button>
-                <button className='btn'>Home</button>
+        <div className='sticky top-0 z-50 bg-base-100 shadow'>
+            <div className="navbar container mx-auto p-4">
+                <div className="flex-1">
+                    <a className="text-2xl font-bold">Keen<span className='text-[#244D3F]'>Keeper</span></a>
+                </div>
+                <div className="flex-none">
+                    <NavLink to={'/'}
+                        className={({ isActive }) => `btn font-semibold text-gray-400 mr-3 ${isActive ? "text-white bg-[#244D3F]" :
+                            "text-black border-gray-200"}`}><FaHome></FaHome> Home</NavLink>
+
+                    <NavLink to={'/timeline'}
+                        className={({ isActive }) => `btn font-semibold text-gray-400 mr-3 ${isActive ? "text-white bg-[#244D3F] " :
+                            "text-black border border-gray-200"}`}><IoTimeOutline></IoTimeOutline> TimeLine</NavLink>
+
+                    <NavLink to={'/stats'}
+                        className={({ isActive }) => `btn font-semibold text-gray-400 mr-3 ${isActive ? "text-white bg-[#244D3F]" :
+                            "text-black border border-gray-200"}`}><TfiStatsUp></TfiStatsUp> Stats</NavLink>
+                </div>
             </div>
         </div>
     );
--- src/pages/error/ErrorPage.jsx
+++ src/pages/error/ErrorPage.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router';
+import { FaHome } from "react-icons/fa";
 
 const ErrorPage = () => {
     return (
         <div>
-            page is not add
+            <div className="min-h-screen flex items-center justify-center bg-gray-100 px-6">
+                <div className="text-center">
+                    <h1 className="text-8xl font-extrabold text-gray-800">404</h1>
+
+                    <h2 className="mt-4 text-2xl font-semibold text-gray-700">
+                        Page Not Found
+                    </h2>
+
+                    <p className="mt-2 text-gray-500 max-w-md mx-auto">
+                        Oops! The page you are looking for doesn’t exist or has been moved.
+                        Please check the URL or go back to the homepage.
+                    </p>
+
+                    <div className="mt-6">
+                        <Link
+                            to="/"
+                            className="px-6 py-3 bg-red-500 text-white rounded-lg shadow hover:bg-[#244D3F] transition"
+                        > Go to Home </Link>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
